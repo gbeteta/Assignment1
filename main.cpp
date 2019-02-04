148fc3258a6b3a883647e2959782690ea582d7d9
gbeteta <gb15d@my.fsu.edu>
2019-02-03 22:47:25 -0500

    Updated main.cpp
    Modified the main program to run the new weapons.

--- main.cpp
+++ main.cpp
@@ -39,6 +39,18 @@ int main(int argc, char** argv) {
     simulateWeapon(weapon, armor);
     delete(weapon);
 
+    weapon = WeaponFactory::getInstance()->getWeapon("hammer");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+
+    weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+
+    weapon = WeaponFactory::getInstance()->getWeapon("wacky axe");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+
     return 0;
 }
 
